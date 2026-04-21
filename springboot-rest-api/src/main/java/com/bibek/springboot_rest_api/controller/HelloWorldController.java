a3fd3efd0614b3ffec8c778d3e2b3f30bbd22aab
Bibek Yogi <bibekyogi6462@gmail.com>
2026-04-21 14:45:32 +0545

    Add files via upload
    Revised Rest API basic concepts and annotations

---
+++ springboot-rest-api/src/main/java/com/bibek/springboot_rest_api/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.bibek.springboot_rest_api.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+}
